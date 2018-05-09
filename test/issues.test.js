e952788deb4fad3fdabeb57c3fffb66fbfa0c271
pubkey <daniel_van_meyer@hotmail.de>
2018-05-10 01:57:47 +0200

    ADD another test for #3

--- test/issues.test.js
+++ test/issues.test.js
@@ -31,5 +31,18 @@ describe('issues.test.js', () => {
         );
         assert.equal(recKey, ident.publicKey);
     });
+    it('#3 issuecommet-387616789', () => {
+        const message = 'foobar';
+        const messageHash = EthCrypto.hash.keccak256(message);
+        const signature = EthCrypto.sign(
+            '0x107be946709e41b7895eea9f2dacf998a0a9124acbb786f0fd1a826101581a07', // privateKey
+            messageHash // hash of message
+        );
+        const signer = EthCrypto.recover(
+            signature,
+            messageHash
+        );
+        assert.ok(signer);
+    });
 
 });
