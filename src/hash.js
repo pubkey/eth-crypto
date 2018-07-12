826e9443f610ebb31765bea67e7a6a781d11fd50
pubkey <daniel_van_meyer@hotmail.de>
2018-07-12 12:19:35 +0200

    CHANGE web3js to etherjs

--- src/hash.js
+++ src/hash.js
@@ -1,15 +1,21 @@
 import {
-    soliditySha3
-} from 'web3-utils';
+    keccak256 as solidityKeccak256
+} from 'ethers/utils/solidity.js';
+
 
 export function keccak256(params) {
+    const types = [];
+    const values = [];
     if (!Array.isArray(params)) {
-        params = [{
-            type: 'string',
-            value: params
-        }];
+        types.push('string');
+        values.push(params);
+    }else {
+        params.forEach(p => {
+            types.push(p.type);
+            values.push(p.value);
+        });
     }
-    return soliditySha3(...params);
+    return solidityKeccak256(types, values);
 }
 
 export const SIGN_PREFIX = '\x19Ethereum Signed Message:\n32';
--- src/tx-data-by-compiled.js
+++ src/tx-data-by-compiled.js
@@ -1,24 +1,18 @@
-import * as Web3EthContract from 'web3-eth-contract';
+import Contract from 'ethers/contracts/contract.js';
 
 export default function txDataByCompiled(
     abi,
     bytecode,
     args
 ) {
-
     // solc returns a string which is often passed instead of the json
     if (typeof abi === 'string') abi = JSON.parse(abi);
 
-    const web3Contract = new Web3EthContract.default(
+    const deployTransaction = Contract.getDeployTransaction(
+        '0x' + bytecode,
         abi,
-        null, {
-            data: '0x' + bytecode
-        }
+        ...args
     );
 
-    const createCode = web3Contract.deploy({
-        arguments: args
-    }).encodeABI();
-
-    return createCode;
+    return deployTransaction.data;
 }
