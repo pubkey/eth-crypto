b9d22e8a49d7e9f33d9b3fca5a9192130b3233bf
pubkey <8926560+pubkey@users.noreply.github.com>
2025-07-01 16:46:56 +0200

    3.1.0

--- dist/es/sign-transaction.js
+++ dist/es/sign-transaction.js
@@ -1,15 +1,48 @@
-import { Transaction } from '@ethereumjs/tx';
+import _asyncToGenerator from "@babel/runtime/helpers/asyncToGenerator";
+import _regeneratorRuntime from "@babel/runtime/regenerator";
+import { createTxFromRPC } from '@ethereumjs/tx';
+import { bytesToHex } from '@ethereumjs/util';
 import { publicKeyByPrivateKey } from './public-key-by-private-key';
 import { toAddress as addressByPublicKey } from './public-key';
-export function signTransaction(rawTx, privateKey) {
-  var txOptions = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-  // check if privateKey->address matches rawTx.from
-  var publicKey = publicKeyByPrivateKey(privateKey);
-  var address = addressByPublicKey(publicKey);
-  if (address != rawTx.from) throw new Error('EthCrypto.signTransaction(): rawTx.from does not match the address of the privateKey');
-  var privateKeyBuffer = Buffer.from(privateKey.replace(/^.{2}/g, ''), 'hex');
-  var tx = Transaction.fromTxData(rawTx, txOptions);
-  var signedTx = tx.sign(privateKeyBuffer);
-  var serializedTx = signedTx.serialize().toString('hex');
-  return serializedTx;
+export function signTransaction(_x, _x2) {
+  return _signTransaction.apply(this, arguments);
+}
+function _signTransaction() {
+  _signTransaction = _asyncToGenerator(/*#__PURE__*/_regeneratorRuntime.mark(function _callee(rawTx, privateKey) {
+    var txOptions,
+      publicKey,
+      address,
+      privateKeyBuffer,
+      tx,
+      signedTx,
+      serializedTx,
+      _args = arguments;
+    return _regeneratorRuntime.wrap(function (_context) {
+      while (1) switch (_context.prev = _context.next) {
+        case 0:
+          txOptions = _args.length > 2 && _args[2] !== undefined ? _args[2] : {};
+          // check if privateKey->address matches rawTx.from
+          publicKey = publicKeyByPrivateKey(privateKey);
+          address = addressByPublicKey(publicKey);
+          if (!(address != rawTx.from)) {
+            _context.next = 1;
+            break;
+          }
+          throw new Error('EthCrypto.signTransaction(): rawTx.from does not match the address of the privateKey');
+        case 1:
+          privateKeyBuffer = Buffer.from(privateKey.replace(/^.{2}/g, ''), 'hex');
+          _context.next = 2;
+          return createTxFromRPC(rawTx, txOptions);
+        case 2:
+          tx = _context.sent;
+          signedTx = tx.sign(privateKeyBuffer);
+          serializedTx = bytesToHex(signedTx.serialize());
+          return _context.abrupt("return", serializedTx);
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }, _callee);
+  }));
+  return _signTransaction.apply(this, arguments);
 }
--- dist/lib/sign-transaction.js
+++ dist/lib/sign-transaction.js
@@ -1,21 +1,55 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.signTransaction = signTransaction;
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 var _tx = require("@ethereumjs/tx");
+var _util = require("@ethereumjs/util");
 var _publicKeyByPrivateKey = require("./public-key-by-private-key");
 var _publicKey = require("./public-key");
-function signTransaction(rawTx, privateKey) {
-  var txOptions = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-  // check if privateKey->address matches rawTx.from
-  var publicKey = (0, _publicKeyByPrivateKey.publicKeyByPrivateKey)(privateKey);
-  var address = (0, _publicKey.toAddress)(publicKey);
-  if (address != rawTx.from) throw new Error('EthCrypto.signTransaction(): rawTx.from does not match the address of the privateKey');
-  var privateKeyBuffer = Buffer.from(privateKey.replace(/^.{2}/g, ''), 'hex');
-  var tx = _tx.Transaction.fromTxData(rawTx, txOptions);
-  var signedTx = tx.sign(privateKeyBuffer);
-  var serializedTx = signedTx.serialize().toString('hex');
-  return serializedTx;
+function signTransaction(_x, _x2) {
+  return _signTransaction.apply(this, arguments);
+}
+function _signTransaction() {
+  _signTransaction = (0, _asyncToGenerator2["default"])(/*#__PURE__*/_regenerator["default"].mark(function _callee(rawTx, privateKey) {
+    var txOptions,
+      publicKey,
+      address,
+      privateKeyBuffer,
+      tx,
+      signedTx,
+      serializedTx,
+      _args = arguments;
+    return _regenerator["default"].wrap(function (_context) {
+      while (1) switch (_context.prev = _context.next) {
+        case 0:
+          txOptions = _args.length > 2 && _args[2] !== undefined ? _args[2] : {};
+          // check if privateKey->address matches rawTx.from
+          publicKey = (0, _publicKeyByPrivateKey.publicKeyByPrivateKey)(privateKey);
+          address = (0, _publicKey.toAddress)(publicKey);
+          if (!(address != rawTx.from)) {
+            _context.next = 1;
+            break;
+          }
+          throw new Error('EthCrypto.signTransaction(): rawTx.from does not match the address of the privateKey');
+        case 1:
+          privateKeyBuffer = Buffer.from(privateKey.replace(/^.{2}/g, ''), 'hex');
+          _context.next = 2;
+          return (0, _tx.createTxFromRPC)(rawTx, txOptions);
+        case 2:
+          tx = _context.sent;
+          signedTx = tx.sign(privateKeyBuffer);
+          serializedTx = (0, _util.bytesToHex)(signedTx.serialize());
+          return _context.abrupt("return", serializedTx);
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }, _callee);
+  }));
+  return _signTransaction.apply(this, arguments);
 }
